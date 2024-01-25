fe57ee6fa4a47a964fac70b56890685cbc486c3d
Aeden Leal <83915771+snailpi1@users.noreply.github.com>
2024-01-25 18:25:28 +0100

    IT WORKS

--- exampleTLEMiciusParis.py
+++ exampleTLEMiciusParis.py
@@ -18,6 +18,7 @@ tle_line_2 = "2 41731  97.3499  30.8507 0012844 347.0485 124.2616 15.25507799261
 tle = (tle_line_1, tle_line_2)
 
 parisparams = [48.8566, 2.3522, 80, "Paris"]
+niceparams = [43.6274, 7.2991, 1200, "Nice"]
 
 micius = ns.Satellite(tle, simType= "tle")
 
@@ -42,16 +43,16 @@ results_nice = TESTCHANNEL_nice.calculateChannelParameters(datetime_list)
 #             - timeList (datetime)
 
 
-filtered_timelist_paris = (results_paris[2])[index]
-filtered_elevation_paris = (results_paris[1])[index]
-filtered_elevation_nice = (results_nice[1])[index]
+filtered_timelist_paris = (results_paris[2])#[index]
+filtered_elevation_paris = (results_paris[1])#[index]
+filtered_elevation_nice = (results_nice[1])#[index]
 
 plt.figure(dpi=600)
 plt.title("Elevation as a Function of Time")
 plt.ylabel("Elevation (degrees)")
 plt.xlabel("Time (GMT+1)")
-plt.plot(filtered_timelist, filtered_elevation_paris)
-plt.plot(filtered_timelist, filtered_elevation_nice, color="#710193")
+plt.plot(filtered_timelist_paris, filtered_elevation_paris)
+plt.plot(filtered_timelist_paris, filtered_elevation_nice, color="#710193")
 plt.grid(color="gray")
 
 elevations_satellite_paris = 90-filtered_elevation_paris
@@ -68,13 +69,13 @@ zenith_angles_nice = data_nice_1550[:, 0]
 transmittance_values_nice = data_nice_1550[:, 1]
 
 transmittance_paris_interpolated = np.interp(elevations_satellite_paris, zenith_angles_paris, transmittance_values_paris)
-transmittance_nice_interpolated = np.interp(elevations_satellite_nice, zenith_nice, transmittance_values, paris)
+transmittance_nice_interpolated = np.interp(elevations_satellite_nice, zenith_angles_nice, transmittance_values_nice)
 
 plt.figure()
 plt.title("Transmittance as a Function of Time")
 plt.ylabel("Transmittance")
 plt.xlabel("Time (GMT+1)")
-plt.plot(filtered_timelist, transmittance_paris_interpolated)
-plt.plot(filtered_timelist, transmittance_nice_interpolated, color="#710193")
+plt.plot(filtered_timelist_paris, transmittance_paris_interpolated)
+plt.plot(filtered_timelist_paris, transmittance_nice_interpolated, color="#710193")
 plt.grid(color="gray")
 plt.show()
