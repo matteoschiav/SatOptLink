6d542b1307376679048524e2f97cd7c1ab1aeea6
Aeden Leal <83915771+snailpi1@users.noreply.github.com>
2024-01-25 18:18:57 +0100

    Using V's values for Paris at Nice at 1550nm

--- exampleTLEMiciusParis.py
+++ exampleTLEMiciusParis.py
@@ -29,35 +29,52 @@ TESTCHANNEL_paris = ns.SimpleDownlinkChannel(micius, paris)
 # Calculating the channel parameters for the datetime list
 results_paris = TESTCHANNEL_paris.calculateChannelParameters(datetime_list)
 
+nice = ns.GroundStation(*niceparams)
+
+# Creating the channel
+TESTCHANNEL_nice = ns.SimpleDownlinkChannel(micius, nice)
+
+# Calculating the channel parameters for the datetime list
+results_nice = TESTCHANNEL_nice.calculateChannelParameters(datetime_list)
+
 #             - length [m]
 #             - elevation [degrees]
 #             - timeList (datetime)
 
-index = np.where(results_paris[1] > 30)
 
-filtered_timelist = (results_paris[2])[index]
-filtered_elevation = (results_paris[1])[index]
+filtered_timelist_paris = (results_paris[2])[index]
+filtered_elevation_paris = (results_paris[1])[index]
+filtered_elevation_nice = (results_nice[1])[index]
 
 plt.figure(dpi=600)
 plt.title("Elevation as a Function of Time")
 plt.ylabel("Elevation (degrees)")
 plt.xlabel("Time (GMT+1)")
-plt.plot(filtered_timelist, filtered_elevation, color="#710193")
+plt.plot(filtered_timelist, filtered_elevation_paris)
+plt.plot(filtered_timelist, filtered_elevation_nice, color="#710193")
 plt.grid(color="gray")
 
-elevations_satellite = 90-filtered_elevation
+elevations_satellite_paris = 90-filtered_elevation_paris
+elevations_satellite_nice = 90-filtered_elevation_nice
 
-data = np.genfromtxt('transmission_data_corrected1.csv', delimiter=',', skip_header=1)
+data_paris_1550 = np.genfromtxt('transmission_data_Paris.csv', delimiter=',', skip_header=1)
+data_nice_1550 = np.genfromtxt('transmission_data_Nice.csv', delimiter=',', skip_header=1)
 
-zenith_angles = data[:, 0]
-transmittance_values = data[:, 1]
 
-transmittance_interpolated = np.interp(elevations_satellite, zenith_angles, transmittance_values)
+zenith_angles_paris = data_paris_1550[:, 0]
+transmittance_values_paris = data_paris_1550[:, 1]
+
+zenith_angles_nice = data_nice_1550[:, 0]
+transmittance_values_nice = data_nice_1550[:, 1]
+
+transmittance_paris_interpolated = np.interp(elevations_satellite_paris, zenith_angles_paris, transmittance_values_paris)
+transmittance_nice_interpolated = np.interp(elevations_satellite_nice, zenith_nice, transmittance_values, paris)
 
 plt.figure()
 plt.title("Transmittance as a Function of Time")
 plt.ylabel("Transmittance")
 plt.xlabel("Time (GMT+1)")
-plt.plot(filtered_timelist, transmittance_interpolated, color="#710193")
+plt.plot(filtered_timelist, transmittance_paris_interpolated)
+plt.plot(filtered_timelist, transmittance_nice_interpolated, color="#710193")
 plt.grid(color="gray")
 plt.show()
