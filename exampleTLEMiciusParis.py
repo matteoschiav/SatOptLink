79d70cadb24ae2891725ffa21ee7efff505338ff
Aeden Leal <83915771+snailpi1@users.noreply.github.com>
2024-01-25 15:02:11 +0100

    Added a working example using accurately generated data

--- exampleTLEMiciusParis.py
+++ exampleTLEMiciusParis.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 import pandas as pd
 
+# Creating a datetime list for the orbit propagator
 start = pd.Timestamp(datetime.now())
 start = pd.Timestamp(datetime(2024, 1, 23, 23))
 end = pd.Timestamp(datetime.now()+timedelta(hours= 12))
@@ -11,18 +12,21 @@ end = pd.Timestamp(datetime(2024, 1, 24, 1))
 t = np.linspace(start.value, end.value, 100000)
 datetime_list = pd.to_datetime(t)
 
-tle_line_1 = "1 41731U 16051A   24016.15735159  .00011450  00000-0  34540-3 0  9998"
-tle_line_2 = "2 41731  97.3167 289.0989 0012522  59.2544 300.9930 15.34373256413200"
+# Using TLE from 27/07/2021 QSS-Micius
+tle_line_1 = "1 41731U 16051A   21117.42314584  .00000696  00000-0  30260-4 0  9998"
+tle_line_2 = "2 41731  97.3499  30.8507 0012844 347.0485 124.2616 15.25507799261429"
 tle = (tle_line_1, tle_line_2)
 
-parisparams = [48.8566, 2.3522, 35, "Paris"]
+parisparams = [48.8566, 2.3522, 80, "Paris"]
 
 micius = ns.Satellite(tle, simType= "tle")
 
 paris = ns.GroundStation(*parisparams)
 
+# Creating the channel
 TESTCHANNEL_paris = ns.SimpleDownlinkChannel(micius, paris)
 
+# Calculating the channel parameters for the datetime list
 results_paris = TESTCHANNEL_paris.calculateChannelParameters(datetime_list)
 
 #             - length [m]
@@ -40,12 +44,20 @@ plt.ylabel("Elevation (degrees)")
 plt.xlabel("Time (GMT+1)")
 plt.plot(filtered_timelist, filtered_elevation, color="#710193")
 plt.grid(color="gray")
-plt.xticks(rotation=45)
 
-zenith_transmittance = 0.5
+elevations_satellite = 90-filtered_elevation
 
-filtered_transmittance = zenith_transmittance ** (1/np.sin(np.deg2rad(filtered_elevation)))
+data = np.genfromtxt('transmission_data_corrected1.csv', delimiter=',', skip_header=1)
+
+zenith_angles = data[:, 0]
+transmittance_values = data[:, 1]
+
+transmittance_interpolated = np.interp(elevations_satellite, zenith_angles, transmittance_values)
 
 plt.figure()
-plt.plot(filtered_timelist, filtered_transmittance)
+plt.title("Transmittance as a Function of Time")
+plt.ylabel("Transmittance")
+plt.xlabel("Time (GMT+1)")
+plt.plot(filtered_timelist, transmittance_interpolated, color="#710193")
+plt.grid(color="gray")
 plt.show()
